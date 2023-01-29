692e39d4ed2597410860efa51d4d3802d8d7e04c
Josh Arnold <rncodepoet@gmail.com>
2023-01-29 12:59:14 -0600

    Moved the sample endpoint/request code to a separate file

--- src/__tests__/HttpLambda.test.ts
+++ src/__tests__/HttpLambda.test.ts
@@ -1,65 +1,13 @@
-import { createContainer, createServiceModule, inject } from '@aesop-fables/containr';
-import { IHttpEndpoint, httpPut, getRoute, errorWrapper, convertNullTo200, xssFilter } from '..';
+import { createContainer } from '@aesop-fables/containr';
+import { getRoute } from '..';
 import { createHttpLambda, invokeHttpHandler } from '../HttpLambda';
-import { useMiddleware } from '../Decorators';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpErrorHanlder from '@middy/http-error-handler';
-
-interface CreateStatusAlertRequest {
-  app: string;
-  version: string;
-  region: string;
-  message: string;
-  active: boolean;
-}
-
-interface StatusAlert {
-  id: string;
-  app: string;
-  version: string;
-  region: string;
-  message: string;
-  active: boolean;
-}
-
-interface IRecorder {
-  record(request: CreateStatusAlertRequest): void;
-}
-
-class Recorder implements IRecorder {
-  request?: CreateStatusAlertRequest;
-  record(request: CreateStatusAlertRequest): void {
-    this.request = request;
-  }
-}
-
-const TestServices = {
-  Recorder: 'recorder',
-};
-
-@httpPut('testpath')
-@useMiddleware(errorWrapper, httpJsonBodyParser, xssFilter, convertNullTo200, httpErrorHanlder)
-class CreateStatusAlertEndpoint implements IHttpEndpoint<CreateStatusAlertRequest, StatusAlert> {
-  constructor(@inject(TestServices.Recorder) private readonly recorder: IRecorder) {}
-  async handle(request: CreateStatusAlertRequest): Promise<StatusAlert> {
-    this.recorder.record(request);
-
-    return {
-      id: '123',
-      ...request,
-    };
-  }
-}
-
-const setupCreateHttpLambdaTest = createServiceModule('test', (services) => {
-  services.register(TestServices.Recorder, () => new Recorder());
-});
-
-// Ok, this describe/test block essentially served as debugging
-// It's time to turn this into a meaningful set of tests
-
-// Step 1: Let's make this file a little easier to read by moving all of the setup stuff
-// into a separate file. Right now it's almost impossible to mentally parse this.
+import {
+  CreateStatusAlertRequest,
+  setupCreateHttpLambdaTest,
+  CreateStatusAlertEndpoint,
+  Recorder,
+  TestServices,
+} from './common';
 
 // Step 2: We'll create the test blocks and list out the tests we SHOULD have
 
---
+++ src/__tests__/common/index.ts
@@ -0,0 +1,55 @@
+import { createServiceModule, inject } from '@aesop-fables/containr';
+import { IHttpEndpoint, httpPut, errorWrapper, convertNullTo200, xssFilter } from '../../';
+import { useMiddleware } from '../../Decorators';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpErrorHanlder from '@middy/http-error-handler';
+
+export interface CreateStatusAlertRequest {
+  app: string;
+  version: string;
+  region: string;
+  message: string;
+  active: boolean;
+}
+
+export interface StatusAlert {
+  id: string;
+  app: string;
+  version: string;
+  region: string;
+  message: string;
+  active: boolean;
+}
+
+export interface IRecorder {
+  record(request: CreateStatusAlertRequest): void;
+}
+
+export class Recorder implements IRecorder {
+  request?: CreateStatusAlertRequest;
+  record(request: CreateStatusAlertRequest): void {
+    this.request = request;
+  }
+}
+
+export const TestServices = {
+  Recorder: 'recorder',
+};
+
+@httpPut('testpath')
+@useMiddleware(errorWrapper, httpJsonBodyParser, xssFilter, convertNullTo200, httpErrorHanlder)
+export class CreateStatusAlertEndpoint implements IHttpEndpoint<CreateStatusAlertRequest, StatusAlert> {
+  constructor(@inject(TestServices.Recorder) private readonly recorder: IRecorder) {}
+  async handle(request: CreateStatusAlertRequest): Promise<StatusAlert> {
+    this.recorder.record(request);
+
+    return {
+      id: '123',
+      ...request,
+    };
+  }
+}
+
+export const setupCreateHttpLambdaTest = createServiceModule('test', (services) => {
+  services.register(TestServices.Recorder, () => new Recorder());
+});
