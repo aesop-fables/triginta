46bde8beeb1b4af47675abf94c115f487a265ca5
Josh Arnold <rncodepoet@gmail.com>
2023-03-19 20:22:52 -0500

    Switch to peer dependencies

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { IHandler } from './IHandler';
 import { IHttpEndpoint } from './IHttpEndpoint';
 
 export * from './Decorators';
-// export * from './Middleware';
 export * from './HttpLambda';
 export * from './HttpLambdaServices';
 export { default as RouteRegistry, IRouteRegistry } from './RouteRegistry';
