819ff7d1a462c4b3a9e388dc61e4c9cd645f1b37
Josh Arnold <rncodepoet@gmail.com>
2026-02-11 11:54:10 -0600

    Fix test reporting

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 
 // const esModules = ['axios'].join('|');
+const CI = process.env.CI === 'true';
+
+const reporters = ['default'];
+
+if (CI) {
+  reporters.push('jest-junit');
+}
+
 
 module.exports = {
+  reporters,
   preset: 'ts-jest/presets/js-with-ts',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
